984e54e218d78263a029966a10797e61296de371
Manfred von Teichman <von_teichman.m-tit14@it.dhbw-ravensburg.de>
2017-06-12 13:44:54 +0200

    Implemented logging functionality

--- blackboard_api/blackboard_server.py
+++ blackboard_api/blackboard_server.py
@@ -1,6 +1,8 @@
 import logging.config
+from logging.handlers import RotatingFileHandler
+from time import strftime
 
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, request, jsonify
 
 from blackboard_api import settings
 from blackboard_api.api_1_0.blackboard import ns as blackboard
@@ -8,8 +10,12 @@ from blackboard_api.api_1_0.restplus import api
 from blackboard_api.database import db
 
 app = Flask(__name__)
+
+handler = RotatingFileHandler('app.log', maxBytes=100000, backupCount=3)
 logging.config.fileConfig('logging.conf')
 log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+log.addHandler(handler)
 
 
 @app.errorhandler(404)
@@ -17,9 +23,18 @@ def not_found(error):
     return jsonify(error=str(error)), 404
 
 
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    log.info('%s %s %s %s %s %s',
+             timestamp, request.remote_addr, request.method,
+             request.scheme, request.full_path, response.status)
+    return response
+
+
 def configure_app(flask_app):
     pass
-    flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
+    # flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
     flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
@@ -41,8 +56,7 @@ def initialize_app(flask_app):
 
 def main():
     initialize_app(app)
-    log.info('>>>>> Starting development server at http://{}/api/v1 <<<<<'.format(app.config['SERVER_NAME']))
-    app.run(debug=settings.FLASK_DEBUG)
+    app.run(debug=settings.FLASK_DEBUG, host='0.0.0.0', port=1337)
 
 
 if __name__ == '__main__':
